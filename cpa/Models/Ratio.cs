8846d89b52ed73e5d912e08c06facb8cd232e5f9
seanc <smcox@cpp.edu>
2018-01-22 19:20:57 -0800

    Company model done, starting Ratio models

---
+++ cpa/Models/Ratio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cpa.Models
+{
+    public class Ratio
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int UserID { get; set; }
+        //public RatioLines<RatioLine> {get; set;}
+
+        public Ratio(int ID, string Name)
+        {
+            this.ID = ID;
+            this.Name = Name;
+        }
+    }
+}
--- cpa/Models/company.cs
+++ cpa/Models/company.cs
@@ -12,5 +12,29 @@ namespace cpa.Models
         public string Market { get; set; }
         public string Symbol { get; set; }
         public string Description { get; set; }
+
+        public Company(int ID, string Name)
+        {
+            this.ID = ID;
+            this.Name = Name;
+        }
+
+        public Company(int ID, string Name, string Description)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Description = Description;
+        }
+
+        public Company(int ID, string Name, string Description, string Market, string Symbol)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Description = Description;
+            this.Market = Market;
+            this.Symbol = Symbol;
+        }
     }
+
+    
 }
